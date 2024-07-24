caaa7a67d369abc5ab4c21dd1c42da8b3de6a5ed
TM-hasM <160637132+TM-hasM@users.noreply.github.com>
2024-07-24 14:15:34 +0100

    Create caldera-docker.sh

---
+++ caldera-docker.sh
@@ -0,0 +1,4 @@
+git clone https://github.com/mitre/caldera.git --recursive --branch 5.0.0
+cd caldera
+docker-compose build
+docker run -p 7010:7010 -p 7011:7011/udp -p 7012:7012 -p 8888:8888 caldera:server
