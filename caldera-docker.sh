b052e5403fc82ae0839b8493cf761c4c0f068b4c
TM-hasM <160637132+TM-hasM@users.noreply.github.com>
2024-07-25 15:49:30 +0100

    Update caldera-docker.sh

--- caldera-docker.sh
+++ caldera-docker.sh
@@ -1,5 +1,6 @@
 cd /home/ucuser
 git clone https://github.com/mitre/caldera.git --recursive --branch 5.0.0
 cd caldera
-docker-compose build
+sudo docker build --build-arg WIN_BUILD=true . -t caldera:server
 sudo docker run -p 7010:7010 -p 7011:7011/udp -p 7012:7012 -p 8888:8888 caldera:server
+
